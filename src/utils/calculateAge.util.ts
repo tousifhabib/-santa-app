44a9c788b232934f1363fd2587a7fda0e82e532a
Tousif Habib <thabib@mun.ca>
2023-05-16 09:58:40 -0230

    fix(calculateAge.util): Take birthday into account

--- src/utils/calculateAge.util.ts
+++ src/utils/calculateAge.util.ts
@@ -1,8 +1,13 @@
 import logger from '../middlewares/logger.middleware';
 
 export function calculateAge(birthdate: string): number {
-  const birthDateCorrected = birthdate.split('/').reverse().join('-');
-  const age = new Date().getFullYear() - new Date(birthDateCorrected).getFullYear();
+  const [day, month, year] = birthdate.split('/').map(Number);
+  const now = new Date();
+  const thisYear = now.getFullYear();
+  let age = thisYear - year;
+  if (now.getMonth() < month - 1 || (now.getMonth() === month - 1 && now.getDate() < day)) {
+    age--;
+  }
   logger.info(`Calculated age as ${age}`);
   return age;
 }
