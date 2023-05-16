dd6625b25780065e25dd3df79de84c3d95be4005
Tousif Habib <thabib@mun.ca>
2023-05-16 09:59:02 -0230

    test(calculateAge.util.test.ts): Added test

---
+++ test/utils/calculateAge.util.test.ts
@@ -0,0 +1,12 @@
+import { calculateAge } from '../../src/utils/calculateAge.util';
+
+describe('calculateAge', () => {
+    it('should calculate the age based on a birthdate', () => {
+        const birthdate = '01/01/2000';
+        const age = calculateAge(birthdate);
+        const currentYear = new Date().getFullYear();
+        const hasHadBirthday = new Date().getMonth() > 0 || (new Date().getMonth() === 0 && new Date().getDate() >= 1);
+        const expectedAge = hasHadBirthday ? currentYear - 2000 : currentYear - 2000 - 1;
+        expect(age).toEqual(expectedAge);
+    });
+});
