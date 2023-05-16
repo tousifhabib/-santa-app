c8b19fb3e4d510ff67c98db77f7a1010eafa7030
Tousif Habib <thabib@mun.ca>
2023-05-16 08:55:58 -0230

    refactor(error.middleware): Removed redundant console log

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from 'express';
 
 function errorMiddleware(err: Error, req: Request, res: Response) {
-  console.error(err.stack);
   res.status(500).send('Something broke!');
 }
 
