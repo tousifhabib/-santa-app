22330ebf2a489b1fe3858f32b76cd11b3f74ce81
Tousif Habib <thabib@mun.ca>
2023-05-16 08:56:16 -0230

    test(app): Added test for app

---
+++ test/app.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest';
+import { expect } from 'chai';
+import app from '../src/app';
+
+describe('app', () => {
+    it('should be a function', () => {
+        expect(app).to.be.a('function');
+    });
+
+    it('should respond with 500 on unhandled routes', async () => {
+        await request(app)
+            .get('/nonexistent-route')
+            .expect(500);
+    });
+});
