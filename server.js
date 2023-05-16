439fa605e71676eb36b35726daddb510d32cc802
Tousif Habib <thabib@mun.ca>
2023-05-15 23:12:01 -0230

    feat(server.js): Added winston for better server side logging

--- server.js
+++ server.js
@@ -4,7 +4,24 @@ const morgan = require('morgan');
 const axios = require('axios');
 const nodemailer = require('nodemailer');
 const lodash = require('lodash');
+const winston = require('winston');
 const app = express();
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' })
+  ]
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
+}
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -12,19 +29,24 @@ app.use(morgan('dev'));
 app.use(express.static('public'));
 
 let requestList = [];
+
 async function getUserData(username) {
   const usersResponse = await axios.get(`${process.env.API_URL}/users.json`);
+  logger.info(`Retrieving user data for ${username}`);
   return lodash.find(usersResponse.data, { username: username });
 }
 
 async function getProfileData(userUid) {
   const profilesResponse = await axios.get(`${process.env.API_URL}/userProfiles.json`);
+  logger.info(`Retrieved profile data for ${userUid}`);
   return lodash.find(profilesResponse.data, { userUid: userUid });
 }
 
 function calculateAge(birthdate) {
   const birthDateCorrected = birthdate.split("/").reverse().join("-");
-  return new Date().getFullYear() - new Date(birthDateCorrected).getFullYear();
+  const age = new Date().getFullYear() - new Date(birthDateCorrected).getFullYear();
+  logger.info(`Calculated age as ${age}`);
+  return age;
 }
 
 async function sendMail(requestList) {
@@ -53,22 +75,27 @@ async function sendMail(requestList) {
     text: emailContent
   };
 
-  return transporter.sendMail(mailOptions);
+  const result = await transporter.sendMail(mailOptions);
+  logger.info(`Sent mail with result ${result}`);
+  return result;
 }
 
 app.post('/submit', async (req, res) => {
   try {
     const user = await getUserData(req.body.userid);
     if (!user) {
+      logger.warn(`User not found: ${req.body.userid}`);
       return res.status(400).sendFile(__dirname + '/views/error/user-not-registered.html');
     }
 
     const profile = await getProfileData(user.uid);
     if (!profile) {
+      logger.warn(`Profile not found for user: ${user.uid}`);
       return res.status(400).sendFile(__dirname + '/views/error/no-profile-found.html');
     }
 
     if (calculateAge(profile.birthdate) > 10) {
+      logger.warn(`User is older than 10 years: ${user.uid}`);
       return res.status(400).sendFile(__dirname + '/views/error/age-greater-than-10.html');
     }
 
@@ -78,13 +105,16 @@ app.post('/submit', async (req, res) => {
       wish: req.body.wish
     });
 
+    logger.info(`Request received from user: ${user.username}, wish: ${req.body.wish}`);
     return res.sendFile(__dirname + '/views/request-received.html');
   } catch (error) {
+    logger.error(`Error processing request: ${error.message}`);
     return res.status(500).send(error.message);
   }
 });
 
 app.get('/', (req, res) => {
+  logger.info('Main page accessed');
   res.sendFile(__dirname + '/views/index.html');
 });
 
@@ -92,14 +122,14 @@ setInterval(async () => {
   if (requestList.length > 0) {
     try {
       await sendMail(requestList);
-      console.log('Message sent');
+      logger.info('Message sent');
       requestList = [];
     } catch (error) {
-      console.log(error);
+      logger.error(`Error sending email: ${error}`);
     }
   }
 }, 15 * 1000);
 
 const listener = app.listen(process.env.APP_PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port);
+  logger.info('Your app is listening on port ' + listener.address().port);
 });
